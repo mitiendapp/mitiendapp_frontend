cde48aedcf7eeb89c7f0ce84261fcb1eff98e921
count <santiagoosorio310@gmail.com>
2023-06-25 20:41:58 -0500

    Se habilita uso de json, se configura cors, se habilita uso de peticiones, respuestas y errores por parte del cliente

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,34 @@
 import express from 'express';
 import db from './models/index';
+import router from './routes/routes';
+import { json, urlencoded } from 'body-parser';
+import './auth/passport'; 
+import cors from 'cors';
+
 const app = express();
 
+app.use(json());
+app.use(urlencoded({
+    extended:true
+}))
 
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:4200'
+}))
+
+app.use((
+    err:Error,
+    req:express.Request,
+    res:express.Response, 
+    next:express.NextFunction,
+)=>{
+    res.status(500).json({
+        message:err.message
+    })
+})
+
+app.use('/api', router); 
 db.sequelize.sync().then(()=>{
     app.listen(3000, ()=>{
         console.log("Se conecto correctamente");
