5dabf8935d5c59fe6fc45e4c0408f88c8c1d7303
count <santiagoosorio310@gmail.com>
2023-06-25 20:53:12 -0500

    Se actualizan rutas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginRegistroComponent } from './componentes/login-registro/login-registro.component';
 import { ProductosComponent } from './componentes/productos/productos.component';
+import { AuthGuard } from './utils/auth.guard';
 
 const routes: Routes = [
-  {path: '', component: LoginRegistroComponent},
-  {path: 'productos', component: ProductosComponent}
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  }, {
+    path: 'login',
+    component: LoginRegistroComponent,
+  }, { path: 'products', component: ProductosComponent, canActivate: [AuthGuard] },
+  {
+    path: '**',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
