e02151e7bcdcd91b2531dc8930f350268ec34e88
santiago <daniel.lara07c@gmail.com>
2023-06-28 09:44:12 -0500

    se agregan algunas rutas y controladores de producto

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -6,7 +6,7 @@ import { verifyRoles } from "../middlewares/verifyRoles";
 import ROLES_LIST from "../../config/role.list";
 import { loginTest, loginUser } from "../controllers/auth.controller";
 import { verifyJWT } from "../middlewares/verifyJWT";
-import { createProduct, getProductById, getProducts } from "../controllers/product.controller";
+import { createProduct, deleteProductHandler, getProductById, getProducts, updateProductHandler } from "../controllers/product.controller";
 
 const router = Router();
 
@@ -23,7 +23,8 @@ router.post('/user/login', loginUser);
 router.get('/product', getProducts);
 router.get('/test/', getProductById);
 router.post('/product', createProduct)
-
+router.post('/product/delete', deleteProductHandler)
+router.post('/product/update', updateProductHandler)
 
 router.get('/test', passport.authenticate("jwt", { session: false }), loginTest);
 router.get('/test2',verifyJWT, verifyRoles(ROLES_LIST.Admin) , loginTest);
