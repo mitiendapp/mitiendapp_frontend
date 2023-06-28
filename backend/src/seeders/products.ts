1592810afadfa30af549204fe5954445817a0d05
santiago <daniel.lara07c@gmail.com>
2023-06-27 21:03:57 -0500

    .

--- backend/src/seeders/products.ts
+++ backend/src/seeders/products.ts
@@ -10,63 +10,63 @@ export const products = [
       "name": "Samsung Galaxy S21",
       "description": "celular",
       "price": "6.999.000",
-      "image": "https://pngimg.com/uploads/samsung_galaxy_s21/small/samsung_galaxy_s21_PNG9.png",
+      "image": "https://smselectronic.com/wp-content/uploads/2021/01/8-1.jpg",
       "stock": "5"
     },
     {
       "name": "MacBook Pro",
       "description": "laptop",
       "price": "12.999.999",
-      "image": "https://pngimg.com/uploads/macbook_pro/small/macbook_pro_PNG5.png",
+      "image": "https://cdn.shopify.com/s/files/1/0604/8373/1606/products/IMG-104966_31eaf77c-ebb0-4907-8348-ac51ff50107f_1445x.jpg?v=1658325861",
       "stock": "3"
     },
     {
       "name": "Sony PlayStation 5",
       "description": "consola de videojuegos",
       "price": "5.499.999",
-      "image": "https://pngimg.com/uploads/ps5/small/ps5_PNG39.png",
+      "image": "https://http2.mlstatic.com/D_Q_NP_841787-MLA44484414455_012021-O.webp",
       "stock": "10"
     },
     {
       "name": "Samsung 4K Smart TV",
       "description": "televisor",
       "price": "4.499.000",
-      "image": "https://pngimg.com/uploads/samsung_tv/small/samsung_tv_PNG7645.png",
+      "image": "https://images.samsung.com/is/image/samsung/co-uhd-tu8000-un58tu8000kxzl-frontblack-221500926?$650_519_PNG$",
       "stock": "7"
     },
     {
       "name": "Canon EOS R5",
       "description": "cámara",
       "price": "6.799.900",
-      "image": "https://pngimg.com/uploads/canon_eos_r5/small/canon_eos_r5_PNG42.png",
+      "image": "https://i1.adis.ws/i/canon/eos-r5_front_rf24-105mmf4lisusm_square_32c26ad194234d42b3cd9e582a21c99b",
       "stock": "4"
     },
     {
       "name": "Fitbit Versa 3",
       "description": "reloj inteligente",
       "price": "899.900",
-      "image": "https://pngimg.com/uploads/fitbit_versa_3/small/fitbit_versa_3_PNG42.png",
+      "image": "https://www.alkosto.com/medias/811138039813-001-750Wx750H?context=bWFzdGVyfGltYWdlc3w3MDUxMHxpbWFnZS9qcGVnfGFXMWhaMlZ6TDJneU5pOW9Nak12T1RnNU56VTNNREEzTkRZMU5DNXFjR2N8NGYzOWMzN2Y1YjBiYmQ5OTIwOTVmYzBjMWNjZjBmNDBiZGJiZTA2NjUxN2U3MWQ5OTdlNmE5ZDgyN2NjODlmNQ",
       "stock": "6"
     },
     {
       "name": "Apple AirPods Pro",
       "description": "auriculares",
       "price": "649.900",
-      "image": "https://pngimg.com/uploads/airpods_pro/small/airpods_pro_PNG10.png",
+      "image": "https://cdnx.jumpseller.com/gestell-technology/image/33489676/resize/1200/1200?1680050556",
       "stock": "8"
     },
     {
       "name": "Nintendo Switch",
       "description": "consola de videojuegos",
       "price": "2.999.900",
-      "image": "https://pngimg.com/uploads/nintendo_switch/small/nintendo_switch_PNG61.png",
+      "image": "https://panamericana.vtexassets.com/arquivos/ids/409311/consola-nintendo-switch-negra-joy-con-azul-y-rojo-neon-2-45496882174.jpg?v=637681059011870000",
       "stock": "9"
     },
     {
       "name": "DJI Mavic Air 2",
       "description": "drone",
       "price": "3.999.900",
-      "image": "https://pngimg.com/uploads/dji_mavic_air_2/small/dji_mavic_air_2_PNG53.png",
+      "image": "https://cdn.shopify.com/s/files/1/0555/4231/7195/products/dronelandingstore-43_e4001d69-a1d6-45d4-8606-fc63b3673613_1695x.jpg?v=1637519011",
       "stock": "2"
     }
   ];
