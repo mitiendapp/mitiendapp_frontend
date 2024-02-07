fc0307f7de60abf82c88e74b774e02d242b6c4a5
count <santiagoosorio310@gmail.com>
2024-02-07 13:29:05 -0500

    update home component

--- frontend/src/app/componentes/home/home.component.ts
+++ frontend/src/app/componentes/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap'
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,12 +8,20 @@ import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap'
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+
+  thereOffers = new BehaviorSubject<Boolean>(false);
+
   constructor(
     private ngCarousel: NgbCarouselModule
   ){
 
   }
   ngOnInit(): void {
-      
+      if(true){
+        this.thereOffers.next(true);
+      }
+  }
+  thereAreOffers(){
+    return this.thereOffers.asObservable();
   }
 }
