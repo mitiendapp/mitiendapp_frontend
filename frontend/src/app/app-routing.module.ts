43bc12e05af6d67b1a23909279019893951a6460
count <santiagoosorio310@gmail.com>
2024-02-14 14:52:56 -0500

    update build directory

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-import { LoginRegistroComponent } from './componentes/login-registro/login-registro.component';
 import { ProductosComponent } from './componentes/productos/productos.component';
 import { AuthGuard } from './utils/auth.guard';
 import { CarritoCompraComponent } from './componentes/carrito-compra/carrito-compra.component';
