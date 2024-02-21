197429a0600bf4c3ffb2d309f9a4c82ea5099824
jhonGs09 <jhonjag1980@gmail.com>
2024-02-21 00:11:58 -0500

    vista-registro-emprendimiento

---
+++ frontend/src/app/services/company.service.ts
@@ -0,0 +1,28 @@
+// business.service.ts
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Company } from '../interfaces/company';
+import { enviroment } from '../enviroments/enviroment';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CompanyService {
+  private endpoint: string;
+  private apiUrl: string;
+  
+
+  constructor(
+    public router: Router,
+    private http: HttpClient) { 
+      this.endpoint = enviroment.endpoint;
+      this.apiUrl = 'companyRegister';}
+
+  signIn(company: Company): Observable<any> {
+    return this.http.post(`${this.endpoint}${this.apiUrl}`, company);
+  }
+}
+
