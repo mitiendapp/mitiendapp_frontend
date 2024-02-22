aa3b200874c07676ac34f1f4809c786706fb3044
marlon <mstick2607@gmail.com>
2024-02-22 00:34:07 -0500

    registro de emprededor funcional

--- frontend/src/app/interfaces/company.ts
+++ frontend/src/app/interfaces/company.ts
@@ -1,11 +1,12 @@
 export interface Company {
-    // document: string;
+    document: string;
     firstName: string;
-    // lastName: string;
-    // nameEmprendimiento: string;
+    lastName: string;
+    nameEmprendimiento: string;
     email: string;
-    // address: string;
-    // phoneNumber: string;
-    // description: string;
+    address: string;
+    phoneNumber: string;
+    description: string;
     password:string;
+    confirmPassword:string;
   }
