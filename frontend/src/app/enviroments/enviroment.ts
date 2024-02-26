87b5c55b56567640d19b32f0d4f8e6929505d82c
marlon <mstick2607@gmail.com>
2024-02-25 22:13:02 -0500

    modificacion de endpoint

--- frontend/src/app/enviroments/enviroment.ts
+++ frontend/src/app/enviroments/enviroment.ts
@@ -1,6 +1,6 @@
 export const enviroment = {
 
     production: false,
-    endpoint:'http://localhost:3100/api/',
+    endpoint:'https://pruebabackend1.onrender.com/api/',
     HTTPS_endpoint: ' https://d43d-152-202-200-21.ngrok.io/api/'
 }
