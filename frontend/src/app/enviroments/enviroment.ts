94fe1f3ea123e17b738c1c0a6b81ea59935581d6
unknown <josealvarez1654@outlook.com>
2024-02-27 17:48:28 -0500

    creacion de variables

--- frontend/src/app/enviroments/enviroment.ts
+++ frontend/src/app/enviroments/enviroment.ts
@@ -3,7 +3,7 @@ export const enviroment = {
     production: false,
     url_validadImagen:'https://mitiendapp-imganalisis.onrender.com',
     url_chat:'https://mitiendapp-chat-1.onrender.com',
-    //
+    //hola
     endpoint:'https://pruebabackend1.onrender.com/api/',
     HTTPS_endpoint: ' https://d43d-152-202-200-21.ngrok.io/api/'
 }
