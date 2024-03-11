b7662b4a35a35af7125c2ad8716f66e3178ba6e7
jhonGs09 <jhonjag1980@gmail.com>
2024-03-10 22:21:09 -0500

    modificacion de vista detalle

--- frontend/src/app/componentes/detalle/detalle.component.ts
+++ frontend/src/app/componentes/detalle/detalle.component.ts
@@ -9,6 +9,8 @@ import { PaymentService } from 'src/app/services/payment.service';
 import { BehaviorSubject, firstValueFrom } from 'rxjs';
 import { kMaxLength } from 'buffer';
 import { enviroment } from 'src/app/enviroments/enviroment';
+import { CartService } from 'src/app/services/cart.service';
+import { MessageService } from 'src/app/services/message.service';
 // pdfMake.vfs = pdfFonts.pdfMake.vfs;
 (<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
 
@@ -20,10 +22,13 @@ import { enviroment } from 'src/app/enviroments/enviroment';
 export class DetalleComponent implements OnInit {
   detail:any;
   idProduct: number = 0;
-
+  cartIcon:any = "../../../assets/icons/shopping-cart-add-svgrepo-com.svg";
+  product: any;
   constructor(private routeActivate: ActivatedRoute,
     private _productService: ProductService,
     private _paymentService: PaymentService,
+    public _cartService: CartService,
+    private messageService: MessageService,
     private route: ActivatedRoute, private router: Router) {}
 
   generatePDF(nombre: string, precio: number) {
@@ -57,30 +62,61 @@ export class DetalleComponent implements OnInit {
 
     // })
   }
-
   ngOnInit(): void {
     this.idProduct = this.routeActivate.snapshot.params["id"];
     this.getProduct(this.idProduct);
   }
+
   getProduct(id:number) {
     this._productService.getProductById(id).subscribe((data: any) => {
       this.detail = data.data;
-    })
+    });
+  }
+ 
+  addToCart(product: Product) {
+    this._cartService.addProduct(this._cartService.productToProductDTO(product));
+    this.messageService.msgSuccess({ message: "El producto fue agregado al carrito correctamente" });
   }
 
-  // createOrder(){
-  //   this._paymentService.createOrder(this.detail).subscribe((data:any)=>{
-  //     console.log(data);
-  //     window.location.href = data.init_point
-  //   })
-  // }
-  createOrder(){
+  removeCart(product: Product) {
+    this._cartService.deleteProduct(this._cartService.productToProductDTO(product));
+    this.messageService.msgSuccess({ message: "El producto fue eliminado al carrito correctamente" });
+  }
+
+  createOrder() {
     this._paymentService.prepareOrder(this.detail).then(async (data)=>{
       console.log(data);
       let order = await firstValueFrom(this._paymentService.createOrder(data));
       window.location.href = `https://checkout.wompi.co/l/${order.payment}`
     })
-    
   }
 
 }
+//   ngOnInit(): void {
+//     this.idProduct = this.routeActivate.snapshot.params["id"];
+//     this.getProduct(this.idProduct);
+//   }
+//   getProduct(id:number) {
+//     this._productService.getProductById(id).subscribe((data: any) => {
+//       this.detail = data.data;
+//     })
+//   }
+ 
+//   addToCart(product: Product) {
+//     this._cartService.addProduct(this._cartService.productToProductDTO(product));
+//     this.messageService.msgSuccess({message: "El producto fue agregado al carrito correctamente"});
+// }
+// removeCart(product: Product) {
+//   this._cartService.deleteProduct(this._cartService.productToProductDTO(product));
+//   this.messageService.msgSuccess({ message: "El producto fue eliminado al carrito correctamente" })
+// }
+//   createOrder(){
+    // this._paymentService.prepareOrder(this.detail).then(async (data)=>{
+    //   console.log(data);
+    //   let order = await firstValueFrom(this._paymentService.createOrder(data));
+    //   window.location.href = `https://checkout.wompi.co/l/${order.payment}`
+    // })
+    
+//   }
+
+
