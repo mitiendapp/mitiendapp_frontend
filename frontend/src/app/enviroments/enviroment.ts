56279ba8f2e7a14baa4c9180bb26d4de65ed15d5
marlon <mstick2607@gmail.com>
2024-03-17 19:58:30 -0500

    ya crea pructos fecha 17/03/2024

--- frontend/src/app/enviroments/enviroment.ts
+++ frontend/src/app/enviroments/enviroment.ts
@@ -4,7 +4,7 @@ export const enviroment = {
     url_validadImagen:'https://mitiendapp-imganalisis.onrender.com',
     url_chat:'https://mitiendapp-chat-1.onrender.com',
     //http://localhost:3600/api/  https://pruebabackend1.onrender.com/api/ https://mitiendapp-server.onrender.com/api/
-    endpoint:'https://mitiendapp-server.onrender.com/api/', 
+    endpoint:'http://localhost:3000/api/', 
     chat_endpoint:'http://localhost:3000/api/',
     url_image_ia:'https://mitiendapp-imganalisis.onrender.com/',
     HTTPS_endpoint:' https://d43d-152-202-200-21.ngrok.io/api/'
